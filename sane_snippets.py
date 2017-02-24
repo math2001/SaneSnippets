15041a1629e93d32c89efdcb48488f205d2fad9a
Mathieu PATUREL <australie.p@gmail.com>
2017-02-25 09:27:26 +1100

    add migrate command

--- sane_snippets.py
+++ sane_snippets.py
@@ -32,6 +32,41 @@ class SaneSnippetsCommand(sublime_plugin.ApplicationCommand):
     def clean_action(self):
         clean()
 
+    def migrate_action(self, soft=None):
+        """Create .sane-snippet from .sublime-snippet
+        if soft is None, then it'll ask each time the destination exists to overwrite it
+        if soft is True, then it won't do anything if the destination already exists
+        if soft is False, then it'll *silently* write the destination, even if it already exists
+        """
+        if soft not in (None, False, True):
+            raise ValueError("The soft arguments should be None, False or True,"
+                             "got {!r}".format(soft))
+
+        for dirname, dirs, files in walk_tree(sublime.packages_path()):
+            for file in files:
+                if not file.endswith('.sublime-snippet'):
+                    continue
+                snippet = Snippet(os.path.join(dirname, file))
+                if soft is True or soft is None:
+                    try:
+                        snippet.convert()
+                    except FileExistsError:
+                        if soft is None:
+                            ans = sublime.yes_no_cancel_dialog(
+                                "The file '{}' already exists. Overwrite?\n\n "
+                                "(press cancel to stop everything)".format(snippet.get_dst()),
+                                'Overwrite', 'Skip')
+                            if ans == sublime.DIALOG_YES:
+                                snippet.convert(force=True)
+                            elif ans == sublime.DIALOG_NO:
+                                continue
+                            elif ans == sublime.DIALOG_CANCEL:
+                                return
+
+                elif soft is False:
+                    snippet.convert(force=True)
+
+
     def run(self, action, *args, **kwargs):
         try:
             function = getattr(self, action + '_action')
